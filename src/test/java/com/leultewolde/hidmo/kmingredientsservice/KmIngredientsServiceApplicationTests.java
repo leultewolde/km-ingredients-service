f059c2d2aba7af29b4db0a1a396f5d8ad976bebb
leultewolde <leulwoldegabriel@gmail.com>
2025-06-20 23:00:55 -0400

    chore: remove unused SpringApplication import to satisfy Sonar rule S1128
    Cleaned up KmIngredientsServiceApplicationTests by removing an unused import
    (org.springframework.boot.SpringApplication) flagged by SonarQube.
    
    Closes #2

--- src/test/java/com/leultewolde/hidmo/kmingredientsservice/KmIngredientsServiceApplicationTests.java
+++ src/test/java/com/leultewolde/hidmo/kmingredientsservice/KmIngredientsServiceApplicationTests.java
@@ -1,23 +1,19 @@
 package com.leultewolde.hidmo.kmingredientsservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.context.properties.bind.validation.BindValidationException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.lang.reflect.Field;
-import java.util.Map;
+import org.springframework.context.ApplicationContext;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
-import static org.junit.jupiter.api.Assertions.*;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class KmIngredientsServiceApplicationTests {
+    @Autowired
+    private ApplicationContext context;
 
     @Test
     void contextLoads() {
+        assertThat(context).isNotNull();
     }
 }
