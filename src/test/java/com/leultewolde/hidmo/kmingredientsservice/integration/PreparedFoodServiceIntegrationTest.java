348fd11efc880bca4ba533ec53e6471842cf2589
Leul Tewolde <leulwoldegabriel@gmail.com>
2025-07-06 21:49:23 -0400

    Fix prepared food deletion integration test

--- src/test/java/com/leultewolde/hidmo/kmingredientsservice/integration/PreparedFoodServiceIntegrationTest.java
+++ src/test/java/com/leultewolde/hidmo/kmingredientsservice/integration/PreparedFoodServiceIntegrationTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -22,6 +23,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@TestPropertySource(properties = "spring.jpa.hibernate.ddl-auto=create-drop")
 class PreparedFoodServiceIntegrationTest {
 
     @Autowired
@@ -90,6 +92,6 @@ class PreparedFoodServiceIntegrationTest {
 
         preparedFoodService.deletePreparedFood(foodId);
 
-        assertEquals(new BigDecimal("2"), ingredientService.getById(ingId).getQuantity());
+        assertEquals(0, ingredientService.getById(ingId).getQuantity().compareTo(new BigDecimal("2")));
     }
 }
