94504caf334619cb334dfb2fabba05a340253143
leultewolde <leulwoldegabriel@gmail.com>
2025-07-10 22:06:21 -0400

    refactor: simplify New Relic license key handling in entrypoint script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,7 @@
 #!/bin/sh
 set -e
 
-# Fetch New Relic license key from Hashicorp Vault if VAULT_TOKEN is provided
-if [ -n "$VAULT_TOKEN" ]; then
-  VAULT_URL="${VAULT_ADDR:-https://vault.leultewolde.com}"
-  VAULT_PATH="${VAULT_NEW_RELIC_PATH:-secret/data/newrelic}"
-  LICENSE=$(curl -sf -H "X-Vault-Token: $VAULT_TOKEN" "$VAULT_URL/v1/$VAULT_PATH" | jq -r '.data.data.NEW_RELIC_LICENSE_KEY // .data.data.license_key // empty')
-  if [ -n "$LICENSE" ]; then
-    export NEW_RELIC_LICENSE_KEY="$LICENSE"
-  fi
-fi
+export NEW_RELIC_LICENSE_KEY="$NEW_RELIC_LICENSE_KEY"
 
 exec java -javaagent:/newrelic/newrelic.jar -jar app.jar
 
