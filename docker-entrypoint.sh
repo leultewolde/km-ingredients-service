61e29240c98718813dff35acc575489b9327cf00
Leul Tewolde <leulwoldegabriel@gmail.com>
2025-07-11 18:48:58 -0400

    remove-new-relic-java-setup (#38)
    * refactor: simplify New Relic license key handling in entrypoint script
    
    * remove New Relic Java setup from Docker configuration

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 set -e
 
-if [ -z "$NEW_RELIC_LICENSE_KEY" ]; then
-    echo "Error: NEW_RELIC_LICENSE_KEY is not set. Exiting." >&2
-    exit 1
-fi
-export NEW_RELIC_LICENSE_KEY="$NEW_RELIC_LICENSE_KEY"
-exec java -javaagent:/newrelic/newrelic.jar -jar app.jar
+#export NEW_RELIC_LICENSE_KEY="$NEW_RELIC_LICENSE_KEY"
+
+#exec java -javaagent:/newrelic/newrelic.jar -jar app.jar
+exec java -jar app.jar
 
